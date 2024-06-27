a2c778aacd403794f82b0ae6345b1c700d66ccad
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2024-06-27 12:08:48 +0200

    Add integration tests on Authorization guard

--- src/guards.rs
+++ src/guards.rs
@@ -31,3 +31,36 @@ impl guard::Guard for AuthorizationGuard {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use actix_web::{http, test, web, App, HttpResponse};
+
+    #[actix_web::test]
+    async fn authorization_guard() {
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(crate::guards::Authorization {
+                    token: "foo".to_owned(),
+                }))
+                .route(
+                    "/",
+                    web::route()
+                        .guard(crate::guards::AuthorizationGuard)
+                        .to(HttpResponse::Ok),
+                ),
+        )
+        .await;
+
+        let req = test::TestRequest::default()
+            .insert_header((http::header::AUTHORIZATION, "foo"))
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+
+        let req = test::TestRequest::default()
+            .insert_header((http::header::AUTHORIZATION, "bar"))
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_client_error());
+    }
+}
