65867d7b7a6129b1e042f3977e5c4b90889e5a02
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2024-06-27 11:45:57 +0200

    Scope admin route with the same auth guard

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 mod conf;
 mod handlers;
-mod middleware;
+mod middlewares;
 
-use actix_web::guard::{Guard, GuardContext};
-use actix_web::middleware::Logger;
-use actix_web::web::{get, post, Data};
-use actix_web::{http, main, App, HttpServer};
+use actix_web::{guard, http, main, middleware, web, App, HttpServer};
 use log::info;
 
 use base64::{engine::general_purpose, Engine as _};
@@ -18,9 +15,9 @@ struct Authorization {
 #[derive(Clone)]
 struct AuthorizationGuard;
 
-impl Guard for AuthorizationGuard {
-    fn check(&self, ctx: &GuardContext) -> bool {
-        match ctx.app_data::<Data<Authorization>>() {
+impl guard::Guard for AuthorizationGuard {
+    fn check(&self, ctx: &guard::GuardContext) -> bool {
+        match ctx.app_data::<web::Data<Authorization>>() {
             Some(app_data_authorization) => {
                 match ctx.head().headers().get(http::header::AUTHORIZATION) {
                     Some(token) => {
@@ -51,20 +48,21 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(Authorization {
+            .app_data(web::Data::new(Authorization {
                 token: general_purpose::STANDARD.encode(&format!(
                     "{}:{}",
                     configuration.user, configuration.password
                 )),
             }))
-            .wrap(Logger::default())
-            .route("/", get().to(handlers::welcome))
-            .route("/ping", get().to(handlers::ping))
-            .route("/download", get().to(handlers::download))
-            .route("/upload", get().to(handlers::upload))
-            .route(
-                "/exec",
-                post().guard(AuthorizationGuard).to(handlers::execute),
+            .wrap(middleware::Logger::default())
+            .route("/", web::get().to(handlers::welcome))
+            .route("/ping", web::get().to(handlers::ping))
+            .service(
+                web::scope("/admin")
+                    .guard(AuthorizationGuard)
+                    .route("/download", web::get().to(handlers::download))
+                    .route("/upload", web::get().to(handlers::upload))
+                    .route("/exec", web::post().to(handlers::execute)),
             )
     })
     .bind(address)?
