1648f66b5e3eec397ca5750a13d48763ed296978
Stéphane TRÉBEL (Perso) <stephane.trebel@gmail.com>
2024-06-27 11:08:29 +0200

    Add an integration test for root route

--- src/handlers.rs
+++ src/handlers.rs
@@ -14,6 +14,24 @@ pub async fn welcome() -> HttpResponse {
     HttpResponse::Ok().body("Welcome to Teddy, see ya !")
 }
 
+#[cfg(test)]
+mod tests {
+    use actix_web::{http::header::ContentType, test, web, App};
+
+    use crate::handlers;
+
+    #[actix_web::test]
+    async fn test_index_get() {
+        let app =
+            test::init_service(App::new().route("/", web::route().to(handlers::welcome))).await;
+        let req = test::TestRequest::default()
+            .insert_header(ContentType::plaintext())
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(resp.status().is_success());
+    }
+}
+
 pub async fn ping() -> HttpResponse {
     HttpResponse::Ok().body("pong")
 }
